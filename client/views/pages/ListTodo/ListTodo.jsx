e13932bbedd3a70bb7fc565e29ee1736a789f1e7
Le Thanh <particle4dev01@gmail.com>
2015-05-21 17:31:12 +0700

    fixed ListTasksPage

--- client/views/pages/ListTodo/ListTodo.jsx
+++ client/views/pages/ListTodo/ListTodo.jsx
@@ -4,10 +4,9 @@ var ListTodo = ReactMeteor.createClass({
         return {tasks: ListTasksCollection.find({}, {sort: {updatedAt: -1}}).fetch()}
     },
     renderTask: function(model) {
-        console.log(model._id);
         return <ListTask 
             key={model._id}
-            timeTask = {model.timeTask} 
+            idTask = {model._id} 
             task={model.task}/>;
     },
     handleKeyPress: function(evt) {
@@ -49,14 +48,37 @@ var ListTodo = ReactMeteor.createClass({
 var ListTask = ReactMeteor.createClass({
     handleCloseTask: function(evt) {
         evt.preventDefault();
-        console.log(this.props.timeTask);
+        Meteor.call('removeTask', this.props.idTask, function(err, res){
+            if(err){
+                console.log(err)
+            }
+            else {
+                console.log(res)
+            }
+        });
+    },
+    handleInput: function(evt) {
+        evt.preventDefault();
+        // var task = ListTasksCollection.findOne(this.props.idTask);
+        // if(task){
+        //     var status = task.status;
+        //     if(status == 'active'){
+        //         $(evt.target).addClass('checked');
+        //         console.log(task._id);
+        //     }
+        //     if(status == 'completed') {
+        //         $(evt.target).css('background-color', 'red');
+        //         console.log(task._id);
+        //     }
+        // }
+        $(evt.target).toggleClass('checked');
     },
     render: function() {
         return (
             <div className="item row" style={{padding: '16px'}}>
-                <div className="col col-20 item-checkbox">
-                    <label className="checkbox" style={{right: 'auto'}}>
-                        <input type="checkbox"/>
+                <div className="col col-20 item-checkbox" >
+                    <label className="checkbox" style={{right: 'auto'}}  >
+                        <input type="checkbox" onChange={this.handleInput}/>
                     </label>
                 </div>
                 <div className="col col-67" style={{whiteSpace: 'normal'}}>{this.props.task}</div>
@@ -64,10 +86,10 @@ var ListTask = ReactMeteor.createClass({
                     <i className="icon ion-close" onClick={this.handleCloseTask}></i>
                 </div>
             </div>
+            
         );
     }
 });
-
 /**
  * HELPERS
  */
